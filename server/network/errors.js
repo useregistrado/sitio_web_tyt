797a92595b6b8968de9d7421f1ce300ecd4c0a15
dradav97 <dradav.97@gmail.com>
2021-09-15 23:45:26 -0500

    manage errors of server and personalizated errors 2

---
+++ server/network/errors.js
@@ -0,0 +1,13 @@
+const response = require('./response')
+
+function errors(err, req, res, next) {
+    
+    console.error('[error]', err)
+
+    const message = err.message || 'Internal Server error'
+    const status = err.statusCode || 500
+
+    response.error(req, res, message, status)
+}
+
+module.exports = errors
