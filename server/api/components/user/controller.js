47edd13acfe9ff18c12a0d8213e5336a50976a83
useregistrado <oscar.vargas07@uptc.edu.co>
2021-09-16 00:53:17 -0500

    relve conflicts

--- server/api/components/user/controller.js
+++ server/api/components/user/controller.js
@@ -1,44 +1,40 @@
-const nanoid = require('nanoid')
+const store = require('../../../store/dummys')
 const auth = require('../auth')
-
 const TABLA = 'user'
 
-module.exports = function (injectedStore) {
+module.exports =function (injectedStore) {
     let store = injectedStore
-    if (!store) {
-        store = require('../../../store/dummy')
+    if(!store){
+        store = require('./index')
     }
 
     function list() {
         return store.list(TABLA)
     }
-
-    function get(id) {
-        return store.get(TABLA, id)
+    
+    function get(id){
+        
+        return store.getregistro(TABLA, id)
     }
 
-
     async function upsert(body) {
+        
         const user = {
             name: body.name,
-            username: body.username,
+            username: body.username
         }
-
-        if (body.id) {
-            user.id = body.id
-        } else {
-            user.id = nanoid.nanoid()
-        }
-
-        if (body.password || body.username) {
-            await auth.upsert({
-                id: user.id,
-                username: user.username,
-                password: body.password,
+        
+        if (body.password || body.username){
+            
+            await auth.upsert('user',{
+                id: body.id,
+                username: body.username,
+                password: body.password
+                
             })
+           
         }
 
-        return store.upsert(TABLA, user)
     }
 
     return {
