53291eacf048dac08d8c20bc4f07b2bce50c03ea
dradav97 <dradav.97@gmail.com>
2021-09-15 23:37:29 -0500

    auth module created

---
+++ server/api/components/auth/controller.js
@@ -0,0 +1,48 @@
+const bcrypt = require('bcrypt')
+const auth = require('../../../auth')
+const TABLA = 'auth'
+
+module.exports = function (injectedStore) {
+    let store = injectedStore
+    if (!store) {
+        store = require('../../../store/dummy')
+    }
+
+    async function login(username, password){
+        const data = await store.query(TABLA, { username: username })
+
+        return bcrypt.compare(password, data.password)
+            .then(sonIguales=>{
+                if (sonIguales === true){
+                    // Generar token
+                    return auth.sign(data)
+                }else {
+                    throw new Error('Informacion invalida')
+                }
+                return data
+            })
+        
+    }
+
+    async function upsert(data) {
+        const authData = {
+            id: data.id,
+        }
+
+        if (data.username) {
+            authData.username = data.username
+        }
+
+        if (data.password) {
+            // hash pide la informacion a encriptar y las veces a ejecutar el algortimo de encriptado
+            authData.password = await bcrypt.hash(data.password,3)
+        }
+
+        return store.upsert(TABLA, authData)
+    }
+
+    return {
+        upsert,
+        login,
+    }
+}
---
+++ server/api/components/auth/index.js
@@ -0,0 +1,7 @@
+const store = require('../../../store/dummy')
+const ctrl = require('./controller')
+
+
+module.exports = ctrl(store)
+
+
