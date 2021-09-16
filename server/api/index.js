1b96522dd1eb7b776ebffd17a197fd4672067622
dradav97 <dradav.97@gmail.com>
2021-09-15 23:50:17 -0500

    module users updated, lack update and delete

--- server/api/index.js
+++ server/api/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const config = require('../config.js')
 const user = require('./components/user/network')
 const auth = require('./components/auth/network')
+const errors = require('../network/errors')
 
 const app = express()
 
@@ -12,6 +13,8 @@ app.use(express.urlencoded({ extended: true}))
 //router
 app.use('/api/user', user)
 app.use('/api/auth', auth)
+
+app.use(errors)
 
 app.listen(config.api.port, ()=>{
   console.log('Api escuchando en el puerto ', config.api.port)
--- server/config.js
+++ server/config.js
@@ -1,5 +1,11 @@
 module.exports = {
+
   api: {
-      port: process.env.API_PORT || 3000,
+    port: process.env.API_PORT || 3000,
+  },
+
+  jwt: {
+    secret: process.env.JWT_SECRET || 'notasecret!'
   }
 }
+
