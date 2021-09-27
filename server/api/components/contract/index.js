6f3e7167d071d524551ac2abc7a8dbd6dde59fff
dradav97 <dradav.97@gmail.com>
2021-09-27 14:30:05 -0500

    contract module started

---
+++ server/api/components/contract/index.js
@@ -0,0 +1,9 @@
+const store = require('../../../store/mysql')
+const ctrl = require('./controller')
+
+
+
+
+module.exports = ctrl(store)
+
+
--- server/api/components/plan/controller.js
+++ server/api/components/plan/controller.js
@@ -1,4 +1,4 @@
-const auth = require('../auth')
+
 const TABLA = 'PLAN'
 
 module.exports =function (injectedStore) {
